7020d538244fe80f7d1be766e80909df649d0365
Andrey Vedischev <vedischev76@mail.ru>
2022-06-24 00:04:47 +0500

    add task4 method string

--- task4/Program.cs
+++ task4/Program.cs
@@ -2,11 +2,27 @@
 using static System.Console;
 Clear();
 
-Write("Введите 2 элемента через пробел: ");
-string[] array = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
-WriteLine(array.Length);
-int a1 =int.Parse(array[0]);
-int a2 =int.Parse(array[1]);
+// Write("Введите 2 элемента через пробел: ");
+// string[] array = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+// WriteLine(array.Length);
+// int a1 =int.Parse(array[0]);
+// int a2 =int.Parse(array[1]);
+// WriteLine($"{a1} -> {a2}");
 
-WriteLine($"{a1} -> {a2}");
+Write("Введите элементы массива через пробел: ");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine($"[{String.Join(",",array)}]");
+
+int[] GetArrayFromString(string arrayStr)
+{
+    string[] arS = arrayStr.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[arS.Length];
+    for(int i = 0; i < arS.Length; i++)
+    {
+        result[i] = int.Parse(arS[i]);
+
+    }
+    return result;
+
+}
 
