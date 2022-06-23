0c339eef0c0cdccd13713b6d51a91bc6e997f367
Andrey Vedischev <vedischev76@mail.ru>
2022-06-23 21:51:44 +0500

    add method getpositive

--- task1/Program.cs
+++ task1/Program.cs
@@ -3,8 +3,7 @@ using static System.Console;
 Clear();
 
 int[] array = GetRandomArray(12, -9, 9);
-
-
+WriteLine($"[{String.Join(",",array)}]");
 
 int[] GetRandomArray(int size, int minValue, int maxValue)
 {
@@ -14,4 +13,14 @@ int[] GetRandomArray(int size, int minValue, int maxValue)
         result[i] = new Random().Next(minValue, maxValue + 1);
     }
     return result;
+}
+
+int GetPositiveSum(int[] array)
+{
+    int sum = 0;
+    for(int i = 0; i< array.Length; i++)
+    {
+        sum+= array[i]>0?array[i]:0; 
+    }
+    return sum;
 }
