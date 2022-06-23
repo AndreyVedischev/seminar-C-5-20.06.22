f8504a7d11ed364cab85ef3b8a18a2a68d02149c
Andrey Vedischev <vedischev76@mail.ru>
2022-06-23 21:39:53 +0500

    add method getrandom

---
+++ task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int[] array = GetRandomArray(12, -9, 9);
+
+
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int [] result = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
