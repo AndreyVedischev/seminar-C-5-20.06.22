a52960b4124c570338178f04967f825869def61a
Andrey Vedischev <vedischev76@mail.ru>
2022-06-23 22:41:59 +0500

    add revers array

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+int[] array = GetRandomArray(5,-5,5);
+Write($"[{String.Join(",",array)}] ->");
+GetReverse(array);
+Write($"[{String.Join(",",array)}]");
+
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int [] result = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+void GetReverse(int[] array)
+{
+        for(int i = 0; i< array.Length; i++)
+    {
+        array[i]*= -1;
+    }
+    
+}
