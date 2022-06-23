7eb1da3901f8946134f5239117ee12477b33cdee
Andrey Vedischev <vedischev76@mail.ru>
2022-06-23 23:39:23 +0500

    add task4

---
+++ task4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите 2 элемента через пробел: ");
+string[] array = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+WriteLine(array.Length);
+int a1 =int.Parse(array[0]);
+int a2 =int.Parse(array[1]);
+
+WriteLine($"{a1} -> {a2}");
+
