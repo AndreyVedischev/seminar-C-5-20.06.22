93eb04446231da55cc7c325a0ae2e1c490df767b
Andrey Vedischev <vedischev76@mail.ru>
2022-06-25 00:31:30 +0500

    add task 41

---
+++ task41/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+// Write("Введите 2 элемента через пробел: ");
+// string[] array = ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+// WriteLine(array.Length);
+// int a1 =int.Parse(array[0]);
+// int a2 =int.Parse(array[1]);
+// WriteLine($"{a1} -> {a2}");
+
+Write("Введите элементы массива через пробел: ");
+int[] array = GetArrayFromString(ReadLine());
+WriteLine($"[{String.Join(",",array)}]");
+
+int[] GetArrayFromString(string arrayStr)
+{
+    string[] arS = arrayStr.Split(new char[]{" ",'&',"!","*",','},StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[arS.Length];
+    for(int i = 0; i < arS.Length; i++)
+    {
+        result[i] = int.Parse(arS[i]);
+
+    }
+    return result;
+
+}
+
