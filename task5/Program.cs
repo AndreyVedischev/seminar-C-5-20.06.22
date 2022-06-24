e86c676278c9cffdcbb826155aecd64bf2cadd8c
Andrey Vedischev <vedischev76@mail.ru>
2022-06-24 22:48:13 +0500

    add task 5

---
+++ task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+WriteLine("Введите размер массива: ");
+int size = int.Parse(ReadLine());
+int[] array = FillArray(size, 5, 5);
+
+int[] FillArray(int size, int min, int max)
+{
+    int[]array = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
